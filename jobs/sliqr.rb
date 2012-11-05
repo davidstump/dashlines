8ca12844f3547670fb040b86e814954af4de8fca
Matt Sears <matt@mattsears.com>
2012-11-04 19:13:24 -0500

    Minor text/copy updates.

--- jobs/sliqr.rb
+++ jobs/sliqr.rb
@@ -4,6 +4,6 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
   http = Net::HTTP.new('api.sliqr.com')
   response = http.request(Net::HTTP::Get.new("/v1.0/users/#{ENV['SLIQR_TOKEN']}"))
   score = JSON.parse(response.body)["score"]
-  send_event('sliqr', { current: score, last: 10 })
+  send_event('sliqr', { current: score, last: rand(score) })
 end
 
