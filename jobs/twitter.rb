7d21169993b384f625e0c53653ede02797f7ed88
David Stump <david@davidstump.net>
2016-12-28 17:04:38 -0500

    Updates Twitter job

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,16 +1,16 @@
 require 'twitter'
 
-Twitter.configure do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['TWITTER_OAUTH_SECRET']
+  config.access_token = ENV['TWITTER_OAUTH_TOKEN']
+  config.access_token_secret = ENV['TWITTER_OAUTH_SECRET']
 end
 
 search_term = URI::encode('littlelines')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
-  tweets = Twitter.search("#{search_term}").results
+  tweets = client.search("#{search_term}").take(10)
   if tweets
     tweets.map! do |tweet|
       { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
