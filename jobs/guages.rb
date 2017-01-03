2691a922606b731da15f6352967903c8169781ee
David Stump <david@davidstump.net>
2017-01-03 09:31:52 -0500

    Hard codes Gauges token and removes twitter title

--- jobs/guages.rb
+++ jobs/guages.rb
@@ -1,8 +1,8 @@
 # Ping Guag.es every 10 seconds for the latest traffic details
 SCHEDULER.every '10m', first_in: 0 do
-  ga = Gauges.new(:token => ENV['GAUGES_TOKEN'])
+  ga = Gauges.new(:token => '8e3ba43a359d65a39af555944ba80c6b')
   gauges = Hashie::Mash.new(ga.gauges).gauges
-  site = gauges.detect {|g| g.title == ENV['APP_DOMAIN'] }
+  site = gauges.detect {|g| g.title == 'littlelines.com' }
   people = site.recent_days.map.with_index{ |day, i| {x: i, y: day.people } }.reverse
   views = site.recent_days.map.with_index{ |day, i| {x: i, y: day.views } }.reverse
   send_event('gauges', points: people, pointsTwo: views )
