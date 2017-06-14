f5500fc29f94a4f039048258c2367d15a0e518da
David Stump <david@davidstump.net>
2017-06-14 09:43:16 -0400

    Updates twitter validation check

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -9,14 +9,20 @@ end
 
 search_term = URI::encode('littlelines ruby elixir')
 
-SCHEDULER.every '10m', :first_in => 0 do |job|
+SCHEDULER.every '10s', :first_in => 0 do |job|
   tweets = Twitter.search("#{search_term}").results
-  if tweets
+  if twee5ts && tweets.any?
     tweets.map! do |tweet|
       { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
     end
     send_event('twitter_mentions', comments: tweets)
   else
-    send_event('twitter_mentions', comments: {name: 'davidstump', body: "Littlelines should tweet more. 0.o", avatar: 'https://pbs.twimg.com/profile_images/672429352080945152/3h3IAuD0.png'})
+    placeholder = {
+      name: 'davidstump',
+      body: "Littlelines should tweet more. 0.o",
+      avatar: 'https://pbs.twimg.com/profile_images/672429352080945152/3h3IAuD0.png'
+    }
+
+    send_event('twitter_mentions', comments: [placeholder])
   end
 end
